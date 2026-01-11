2b02c874a0506d881e5c5f7716ae35f817f4d545
mickael <kaelix-agency@hotmail.com>
2026-01-11 23:03:45 +0100

    navigate to the bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ScrollToTop } from './components/common/ScrollToTop'
 import LandingPage from './pages/LandingPage'
 import ServicesPage from './pages/ServicesPage'
 import ContactPage from './pages/ContactPage'
@@ -8,6 +9,7 @@ import TeamPage from './pages/TeamPage'
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/services" element={<ServicesPage />} />
